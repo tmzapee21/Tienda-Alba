cee385f6d343879f8ccb0acc577ac279a0714543
cerbn <ru.quintupil@duocuc.cl>
2024-06-05 23:20:57 -0400

    Primer fork Ruben se agrega css a la pagina

--- tienda.php
+++ tienda.php
@@ -11,6 +11,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="CSS/tienda.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <title>Tienda Oficial de Vidal</title>
@@ -35,7 +36,7 @@ session_start();
 
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="tienda.php">
           <img src="IMG/Colo_a.png" alt="Logo" height="40">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -53,9 +54,9 @@ session_start();
 
           </ul>
           <form class="d-flex" role="search">
-            <input class="form-control me-3" type="search" placeholder="Buscar" aria-label="Search">
-            <button class="btn btn-outline-light" type="submit">Buscar</button>
-          </form>
+    <input class="form-control me-3" type="search" placeholder="Buscar" aria-label="Search" name="q">
+    <button class="btn btn-outline-light" type="submit">Buscar</button>
+</form>
           <div id="UsuarioNew">
 
             <?php
@@ -83,16 +84,13 @@ session_start();
 
     <!-- Cartas Productos-->
 
-    <!-- Cartas Productos-->
 
 <?php
 // Incluye el archivo PHP y obtén los productos
 $productos = include 'productos.php';
 ?>
 
-<!-- Aquí va el resto de tu código HTML -->
 
-<!-- Luego, en el lugar donde quieres mostrar las tarjetas, puedes hacer algo como esto: -->
 <div class="container">
   <div class="row">
   <?php foreach ($productos as $producto): ?>
@@ -105,7 +103,7 @@ $productos = include 'productos.php';
           <p class="card-text">Precio: $<?php echo $producto["Precio"]; ?></p>
           <p class="card-text">Stock: <?php echo $producto["Stock"]; ?></p>
           <input type="number" id="cantidad<?php echo $producto["id"]; ?>" value="1" min="1" max="<?php echo $producto["Stock"]; ?>">
-          <button class="btn btn-primary" onclick="agregarAlCarrito('<?php echo $producto["id"]; ?>', '<?php echo $producto["Nombre_producto"]; ?>', '<?php echo $producto["Descripcion"]; ?>', document.getElementById('cantidad<?php echo $producto["id"]; ?>').value, '<?php echo $producto["Precio"]; ?>')">Agregar al carrito</button>
+          <button class="btn btn-primary" onclick="agregarAlCarrito('<?php echo $producto["id"]; ?>', '<?php echo $producto["Nombre_producto"]; ?>', '<?php echo $producto["Descripcion"]; ?>', document.getElementById('cantidad<?php echo $producto["id"]; ?>').value, '<?php echo $producto["Precio"]; ?>', '<?php echo $producto["Imagen"]; ?>')">Agregar al carrito</button>
         </div>
       </div>
     </div>
@@ -121,15 +119,14 @@ $productos = include 'productos.php';
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="productosCarrito">
-            <!-- Aquí se mostrarán los productos del carrito -->
-          </div>
-          <div class="modal-footer">
-    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-    <a href="factura.html" class="btn btn-primary" onclick="comprar()">Comprar</a>
-</div>
-        </div>
+        <!-- Aquí se mostrarán los productos del carrito -->
+      </div>
+      <div class="modal-footer" id="piso">
+        <a href="factura.html" class="btn5" onclick="comprar()">COMPRAR</a>
       </div>
     </div>
+  </div>
+</div>
 
 
 
@@ -146,65 +143,93 @@ $productos = include 'productos.php';
   </div>
 
   <script>
-var carrito = [];
-var modal = new bootstrap.Modal(document.getElementById('carritoModal'));
+var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-function agregarAlCarrito(id, nombre, descripcion, cantidad, precio) {
-    var productoExistente = carrito.find(producto => producto.id === id);
+function agregarAlCarrito(id, nombre, descripcion, cantidad, precio, imagen) {
+    var idNumerico = Number(id);
+    var productoExistente = carrito.find(producto => producto.id === idNumerico);
 
     if (productoExistente) {
         productoExistente.cantidad += parseInt(cantidad);
     } else {
         var producto = {
-            id: id,
+            id: idNumerico,
             nombre: nombre,
             descripcion: descripcion,
-            cantidad: parseInt(cantidad), // Asegúrate de que la cantidad es un número
-            precio: precio
+            cantidad: parseInt(cantidad),
+            precio: precio,
+            imagen: imagen // Asegúrate de que estás agregando la imagen aquí
         };
 
         carrito.push(producto);
     }
 
-    var productosCarrito = document.getElementById('productosCarrito');
-    productosCarrito.innerHTML = '';
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    mostrarCarrito();
 
-    for (var i = 0; i < carrito.length; i++) {
-        var precioTotal = (carrito[i].cantidad * carrito[i].precio).toFixed(3);
-        productosCarrito.innerHTML += '<p>' + carrito[i].nombre + ' - ' + carrito[i].descripcion + ' - Cantidad: ' + carrito[i].cantidad + ' - Precio: ' + precioTotal + '</p>';
-    }
-
-    // Limpiar el input de cantidad
     document.getElementById('cantidad' + id).value = "1";
 
-    // Alerta de producto agregado
-    alert('Producto agregado al carrito');
-
-    // Muestra el modal
-    modal.show();
+    alert('PRODUCTO AGREGADO');
 }
-</script>
-<script>
+
+function eliminarProducto(id) {
+    var idNumerico = Number(id);
+    carrito = carrito.filter(producto => producto.id !== idNumerico);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    mostrarCarrito();
+}
+
 function mostrarCarrito() {
     var productosCarrito = document.getElementById('productosCarrito');
     productosCarrito.innerHTML = '';
 
-    for (var i = 0; i < carrito.length; i++) {
-        var precioTotal = (carrito[i].cantidad * carrito[i].precio).toFixed(3);
-        productosCarrito.innerHTML += '<p>' + carrito[i].nombre + ' - ' + carrito[i].descripcion + ' - Cantidad: ' + carrito[i].cantidad + ' - Precio: ' + precioTotal + '</p>';
-    }
+    carrito.forEach(producto => {
+        var precioTotal = (producto.cantidad * producto.precio).toFixed(3);
+        productosCarrito.innerHTML += '<div class="producto-carrito"><img src="' + producto.imagen + '" alt="' + producto.nombre + '"><div class="producto-detalles"><span class="producto-texto">' + producto.nombre + '</span><span>' + producto.descripcion + '</span><span>Cantidad: ' + producto.cantidad + '</span><span>Precio: ' + precioTotal + '</span>' + 
+        '<button class="eliminar" data-id="' + producto.id + '" id="btn-eliminar" >Eliminar</button></div></div>';
+    });
+}
 
-    modal.show();
+
+
+
+document.getElementById('productosCarrito').addEventListener('click', function(event) {
+    if (event.target.classList.contains('eliminar')) {
+        eliminarProducto(event.target.getAttribute('data-id'));
+    }
+});
+
+window.onload = function() {
+    localStorage.removeItem('carrito');
 }
 </script>
 
 
-  <script src="carrito.js"></script>
+  
+<script src="carrito.js"></script>
   <script src="JS/musica.js"></script>
+  <script>
+$(document).ready(function(){
+    $("form").on("submit", function(event){
+        event.preventDefault();
+        $.ajax({
+            url: "buscar.php",
+            type: "get",
+            data: $(this).serialize(),
+            success: function(response){
+                // Aquí puedes actualizar los productos en tu página con la respuesta del servidor
+                $(".container .row").html(response);
+            }
+        });
+    });
+});
+</script>
+
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="JS/loader.js"></script>
 </body>
 
